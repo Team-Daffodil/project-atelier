fa0da056244e7fa597047be23bad377eb5dc3746
LukeAnger <anger.luke@gmail.com>
2022-12-01 16:08:32 -0500

    Test commit to see if test runs

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -3,7 +3,10 @@ import axios from 'axios';
 
 const Questions = () => {
   return (
-    <h1>Why did the chicken cross the road?</h1>
+    <div>
+      <h1>Why did the chicken cross the road?</h1>
+      <div>a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup></div>
+    </div>
   )
 }
 
