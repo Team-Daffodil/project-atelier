deceaff47cdc5f8a3e619e2502e5ed160a4f75e0
LukeAnger <anger.luke@gmail.com>
2022-12-01 15:56:48 -0500

    Fixed test for Questions

--- src/components/Questions.test.jsx
+++ src/components/Questions.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { Questions } from './Questions'
+import Questions from './Questions.jsx'
 
 describe('Questions Component', function () {
   it('should have a question', function () {
-    let { getByText } = render(<App />)
+    let { getByText } = render(<Questions />)
     expect(getByText('Why did the chicken cross the road?')).toMatchInlineSnapshot(`
       <h1>
         Why did the chicken cross the road?
