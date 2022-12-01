f650b36fdccef8453a39c536c9c179c89d43e951
LukeAnger <anger.luke@gmail.com>
2022-12-01 16:10:26 -0500

    TEST JEST COMMIT

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import axios from 'axios';
+import React from 'react'
+import axios from 'axios'
 
 const Questions = () => {
   return (
     <div>
       <h1>Why did the chicken cross the road?</h1>
-      <div>a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup></div>
+      <div>
+        a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup>
+      </div>
     </div>
   )
 }
 
-export default Questions;
+export default Questions
