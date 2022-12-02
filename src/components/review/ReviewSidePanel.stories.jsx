caf4ea54a622fa6cf6de00c206a00ebed22fc5d4
Phea Duch <phea.duch@gmail.com>
2022-12-02 17:06:37 -0500

    basic ReviewSidePanel completed

---
+++ src/components/review/ReviewSidePanel.stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReviewSidePanel from './ReviewSidePanel'
+
+export default {
+  component: ReviewSidePanel,
+  title: 'ReviewSidePanel',
+}
+
+const Template = (args) => <ReviewSidePanel {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  productId: 37311,
+}
