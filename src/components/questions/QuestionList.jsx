1b6a1590b9d906de4742f23435a1952013acc164
LukeAnger <anger.luke@gmail.com>
2022-12-06 14:54:39 -0500

    Added conditional rendering to match the business plans requirement of 4 questions being loaded and show more questions button has an in place scroll

--- src/components/questions/QuestionList.jsx
+++ src/components/questions/QuestionList.jsx
@@ -7,12 +7,17 @@ import QuestionListEntry from './QuestionListEntry.jsx'
 
 const QuestionList = () => {
   const [data, setData] = useState([])
+  const [moreQs, setMoreQs] = useState(false)
+  const questionWrapperStyle = { overflowY: 'scroll', height: '500px' }
+  const showMoreQuestionsHandler = (event) => {
+    setMoreQs(!moreQs)
+  }
   useEffect(() => {
     fetch(
       'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe' +
         '/qa/questions' +
-        '?product_id=37314',
-      { headers: { Authorization: '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' } }
+        '?product_id=37325',
+      { headers: { Authorization: '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' } }
     )
       .then((response) => response.json())
       .then((data) => {
@@ -35,15 +40,54 @@ const QuestionList = () => {
       ansQ
     )
 
-    return (
-      <div className="questions-wrapper">
-        {data &&
-          data.map((ele, i) => {
-            console.log('QUESTION: ', ele)
-            return <QuestionListEntry question={ele} key={i} answer={ansQ[i]} />
-          })}
-      </div>
-    )
+    if (data.length < 4) {
+      return (
+        <div className="questions-wrapper">
+          {data &&
+            data.map((ele, i) => {
+              return (
+                <QuestionListEntry question={ele} key={i} answer={ansQ[i]} />
+              )
+            })}
+        </div>
+      )
+    } else if ((data.length > 4) & !moreQs) {
+      return (
+        <div className="questions-wrapper">
+          {data &&
+            data.map((ele, i) => {
+              if (i < 4) {
+                return (
+                  <QuestionListEntry question={ele} key={i} answer={ansQ[i]} />
+                )
+              }
+            })}
+          <button
+            className="button-more-questions"
+            onClick={showMoreQuestionsHandler}
+          >
+            MORE QUESTIONS
+          </button>
+        </div>
+      )
+    } else if ((data.length > 4) & moreQs) {
+      return (
+        <div
+          className="questions-wrapper"
+          style={questionWrapperStyle}
+          onScroll={(e) => console.log(e.nativeEvent)}
+        >
+          {data &&
+            data.map((ele, i) => {
+              if (i < 6) {
+                return (
+                  <QuestionListEntry question={ele} key={i} answer={ansQ[i]} />
+                )
+              }
+            })}
+        </div>
+      )
+    }
   } else {
     <div className="questions-wrapper">
       <div>
