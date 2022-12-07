e36b462aca0793884dc8c2369532bfebd92d26d7
Phea Duch <phea.duch@gmail.com>
2022-12-07 13:10:42 -0500

    BUGFIX: checking error first to fix helpfulness bug

--- sampleData/reviews.js
+++ sampleData/reviews.js
@@ -2,7 +2,7 @@ export const mockData = `
 {
   "product": "1",
   "page": 0,
-  "count": 10,
+  "count": 11,
   "results": [
   {
     "review_id": 0,
@@ -32,7 +32,7 @@ export const mockData = `
     "recommend": true,
     "response": null,
     "body": "Shoes had a noticeably crack/gap between the leather and the shoe toe cap. Customer Service was very helpful. New pair are being shipped to me.",
-    "date": "2022-10-21T00:00:00.000Z",
+    "date": "2022-10-11T00:00:00.000Z",
     "reviewer_name": "midcc",
     "helpfulness": 7,
     "photos": [
@@ -53,7 +53,7 @@ export const mockData = `
     "recommend": true,
     "response": null,
     "body": "I bought 2 pairs of superstar for me and my husband. He loves it a lot. The shoes is simple and basic so that they are easy to mix with every style.",
-    "date": "2022-10-21T00:00:00.000Z",
+    "date": "2020-01-11T00:00:00.000Z",
     "reviewer_name": "CherylNg91",
     "helpfulness": 7,
     "photos": [
@@ -74,7 +74,7 @@ export const mockData = `
     "recommend": true,
     "response": null,
     "body": "Last month I bought a new Adidas originals superstar cm8077 and Deerupt runner. But this is an issue with the superstar.It’s pasting on both pair is ripping off. I am already using Adidas other products too but this purchase of originals especially the iconic superstar is disappointing me because after watching RunDmc’s music videos I purchased it but now i have to see the gaps on both pair every time I wear it.",
-    "date": "2022-10-21T00:00:00.000Z",
+    "date": "2019-10-08T00:00:00.000Z",
     "reviewer_name": "Aneesh24",
     "helpfulness": 5,
     "photos": [
@@ -95,7 +95,7 @@ export const mockData = `
     "recommend": true,
     "response": null,
     "body": "Superstars have been 1 of my favorite running shoes for many years. I remember them from the 1980s but never being able to get them. When they came out again in the late 1990s/early 2000s I started to but them again, and I love them. They are super easy to wear & comfy, and easily accommodate orthotics (I have worn orthotics for over 30 years for flat feet). My Adidas Superstars go well with almost anything - jeans & t-shirt, tracksuit, or shorts. They are great for everyday use and I have found them great for playing tennis as well. They grip the court well and I can change direction easily in them. I am very happy with these shoes!",
-    "date": "2022-10-21T00:00:00.000Z",
+    "date": "2022-10-23T00:00:00.000Z",
     "reviewer_name": "DOCA278",
     "helpfulness": 2,
     "photos": [
@@ -116,7 +116,7 @@ export const mockData = `
     "recommend": true,
     "response": null,
     "body": "I ordered these along with the white pair (black stripes). I love them both. But these made me instantly fall in love! The colors really compliment each other. And they are comfortable. I did size down to a 7.5 women based off the reviews and the advice. And it fits perfectly. I usually wear a 8 women. But no complaints!",
-    "date": "2022-10-21T00:00:00.000Z",
+    "date": "2022-04-17T00:00:00.000Z",
     "reviewer_name": "Terri",
     "helpfulness": 5,
     "photos": [
@@ -137,7 +137,7 @@ export const mockData = `
     "recommend": true,
     "response": null,
     "body": "I love these shoes! I already own a pair of classic white and black Superstars so I decided to get a pair customized. They look great! Very comfortable and will match everything I wear. I recommend going to a store that sells Adidas shoes and trying on Superstars to find your correct size, especially if you're buying women's shoes. I go a full size down (8.5 regular to 7.5 for Adidas) . Customized shoes cannot be returned, even if they don't fit. Overall, great price and worth it to have a unique pair of shoes!",
-    "date": "2022-10-21T00:00:00.000Z",
+    "date": "2022-03-06T00:00:00.000Z",
     "reviewer_name": "JND96",
     "helpfulness": 12,
     "photos": [
@@ -158,7 +158,7 @@ export const mockData = `
     "recommend": true,
     "response": null,
     "body": "I ordered the cream colored SuperStar Shoes with the Adidas overlay on the laces and they look so just as amazing as they do on the website! I immediately love that this color goes with so many of my spring outfits and super comfortable to wear for long walks. I'm a size 8.5 in adidas sneakers, but i went with everyone's recommendations to size down and I'm glad I did because they fit perfect with the size 8. Easy to wipe clean as well. Would love to see more of these styles with other pastel colors for Spring/Summer vibes.",
-    "date": "2022-10-21T00:00:00.000Z",
+    "date": "2020-09-28T00:00:00.000Z",
     "reviewer_name": "FoxyAuxi",
     "helpfulness": 1,
     "photos": [
@@ -179,7 +179,7 @@ export const mockData = `
     "recommend": true,
     "response": null,
     "body": "Who needs to go to Coachella to feel like a superstar...when all you need is these babies to be ready to ROCK! They both sell out real fast..so I bought 2pairs..couldn't get my hands on a single pair for months. Tried adidas stores, random stockists & of course online. These are the coolest shoes & I had to have a pair! You can wear them with everything & anything. As soon as I saw they were available again online.. I wasn't going to miss out..so I bought 2pairs the same! Recommend you superstars do the same if you want the coolest shoe in your wardrobe..I guarantee them to be a fave for years. Time to take these babies for a walk!",
-    "date": "2022-10-21T00:00:00.000Z",
+    "date": "2022-10-11T00:00:00.000Z",
     "reviewer_name": "GeishaGirl",
     "helpfulness": 101,
     "photos": [
@@ -200,7 +200,7 @@ export const mockData = `
     "recommend": true,
     "response": null,
     "body": "Although I love the overall look of these shoes, I am very disappointed with the comfort. I’ve owned adidas superstars before and these are constructed different- the tongue is made of a thin leather and is longer than normal. I wore them once and it cut the front of both my ankles. I spoke to someone from adidas expressing my discomfort and their response was “because I have worn them once, there is nothing they can do”. This response was extremely frustrating, as I would not have uncovered this had I not worn them. Even though I’ve always been a customer and an avid fan of adidas, this experience has me doubting that I’ll ever shop here again.",
-    "date": "2022-10-21T00:00:00.000Z",
+    "date": "2022-05-30T00:00:00.000Z",
     "reviewer_name": "Hmmmmm13",
     "helpfulness": 19,
     "photos": [
@@ -221,7 +221,7 @@ export const mockData = `
     "recommend": true,
     "response": null,
     "body": "Highly recommend this product, awesome quality and comfort",
-    "date": "2022-10-21T00:00:00.000Z",
+    "date": "2022-10-02T00:00:00.000Z",
     "reviewer_name": "Valyy19",
     "helpfulness": 3,
     "photos": [
--- src/lib/apiMockHandlers.js
+++ src/lib/apiMockHandlers.js
@@ -7,26 +7,31 @@ const apiUrl = process.env.API_URL
 export const handlers = [
   rest.get(apiUrl + '/reviews', (req, res, ctx) => {
     const productId = req.url.searchParams.get('product_id')
+    const sort = req.url.searchParams.get('sort')
+    const data = JSON.parse(mockReviewsData)
     if (productId === '1') {
-      return res(ctx.json(JSON.parse(mockReviewsData)))
+      // if (sort === 'newest') {
+      //   data =
+      // }
+      return res(ctx.json(data))
     }
     return res(ctx.status(500))
   }),
 
   rest.put(apiUrl + '/reviews/:reviewId/helpful', (req, res, ctx) => {
     const { reviewId } = req.params
-    if (reviewId === '0') {
-      return res(ctx.status(204))
+    if (reviewId === '99') {
+      return res(ctx.status(500))
     }
-    return res(ctx.status(500))
+    return res(ctx.status(204))
   }),
 
   rest.put(apiUrl + '/reviews/:reviewId/report', (req, res, ctx) => {
     const { reviewId } = req.params
-    if (reviewId === '0') {
-      return res(ctx.status(204))
+    if (reviewId === '99') {
+      return res(ctx.status(500))
     }
-    return res(ctx.status(500))
+    return res(ctx.status(204))
   }),
 
   rest.get(process.env.API_URL + '/reviews/meta', (req, res, ctx) => {
