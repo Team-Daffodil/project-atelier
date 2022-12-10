4f30eae853ad687b9bafa760c33bbf7bdd5d79eb
Phea Duch <phea.duch@gmail.com>
2022-12-09 20:24:36 -0500

    added new props to stories

--- src/components/review/ReviewSidePanel.stories.jsx
+++ src/components/review/ReviewSidePanel.stories.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReviewSidePanel from './ReviewSidePanel'
+import '@testing-library/jest-dom'
 
 export default {
   component: ReviewSidePanel,
   title: 'ReviewSidePanel',
 }
 
-const Template = (args) => <ReviewSidePanel {...args} />
+const Template = (args) => (
+  <ReviewSidePanel {...args} handleSetRating={() => {}} />
+)
 
 export const Default = Template.bind({})
 Default.args = {
