3e087ed7d838fed0ac92c29adabe92308d152fe0
Phea Duch <phea.duch@gmail.com>
2022-12-16 08:11:35 -0500

    added modal and did some css

---
+++ src/components/review/NewFormModal.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ModalContainer = styled.div`
+  width: 900px;
+  height: 720px;
+  background: white;
+  z-index: 10;
+  border-radius: 16px;
+  box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.04);
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: block;
+  overflow: auto;
+  padding: 16px 8px;
+`
+
+const Modal = ({ children, setModalIsOpen }) => {
+  return (
+    <ModalContainer onClick={() => setModalIsOpen(false)}>
+      {children}
+    </ModalContainer>
+  )
+}
+
+export default Modal
